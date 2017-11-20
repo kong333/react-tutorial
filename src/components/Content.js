c4e95001c496b5fca9112d275a819dfc40210a79
이종헌 <>
2017-11-20 17:28:06 +0800

    Initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+/**
+ * Created by kong333 on 2017. 11. 13..
+ */
+import React from 'react';
+
+class Content extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>Contents</h2>
+                <p>hey!</p>
+            </div>
+        )
+    }
+}
+
+export default Content;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Created by kong333 on 2017. 11. 13..
+ */
+
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <h1>Header</h1>
+        );
+    }
+}
+
+export default Header;
+
